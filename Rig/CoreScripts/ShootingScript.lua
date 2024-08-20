3de7b056d826b40bafb89527f6a620273c943e7c
Zèe <170918125+SarkWrk@users.noreply.github.com>
2024-08-19 23:02:21 -0700

    Update ShootingScript.lua

--- Rig/CoreScripts/ShootingScript.lua
+++ Rig/CoreScripts/ShootingScript.lua
@@ -71,6 +71,7 @@ main.Configurations.Attributes = {
 	"EnemyTableUpdateDelay",
 	"ViewDistance",
 	"RaycastParams/FilterType",
+	"ViewRadius",
 }
 
 
@@ -123,17 +124,25 @@ main.RunService.Heartbeat:Connect(function() : nil
 					error(script:GetFullName() .. ".main.RunService.Heartbeat could not identify a part to start the raycast at. The start part can not be changed.")
 				end
 				return
+			end			
+			
+			-- Creates a new CFrame that looks in the direction of the enemy and sets the view distance using main.Configurations.ViewDistance
+			local newView = CFrame.lookAt(startPart.Position, endPart.Position)
+			local viewDirection = newView.LookVector * main.Configurations.ViewDistance
+			
+			-- Compared the viewDirection Y orientation and start part Y orientation so check whether the rig can see the enemy using main.Configurations.ViewRadius, if not: returns
+			local _, startYOrientation, _ = startPart.CFrame:ToEulerAnglesXYZ()
+			local _, endYOrientation, _ = newView:ToOrientation()
+			
+			if math.abs(math.deg(startYOrientation) - math.deg(endYOrientation)) <= main.Configurations.ViewRadius then
+				return
 			end
-			
-			
+
 			-- Sets up the RaycastParams for the raycast
 			local rayCastParams = RaycastParams.new()
 			rayCastParams.FilterType = Enum.RaycastFilterType[main.Configurations.RaycastParams.FilterType]
 			rayCastParams.RespectCanCollide = main.Configurations.RaycastParams.RespectCanCollide
 			rayCastParams.FilterDescendantsInstances = main.Configurations.RaycastParams.IgnoreInViewChecking
-			
-			local viewDirection = CFrame.lookAt(startPart.Position, endPart.Position).LookVector * main.Configurations.ViewDistance --[[
-			^^ Sets the view distance ]]
 			
 			-- At this point, the programme has identified the end part and the start part to raycast to
 			local raycast = game.Workspace:Raycast(startPart.Position, viewDirection, rayCastParams)
@@ -154,6 +163,121 @@ main.RunService.Heartbeat:Connect(function() : nil
 		main.CanSeeEnemy = hasSeenEnemy
 	end
 end)
+
+--[[
+This if statement contains code that is only ran IF main.Configurations.AllowAdjustableSettings is set to true
+]]
+if main.Configurations.AllowAdjustableSettings == true then
+	--[[
+	This function creates attributes based on main.Configurations.Attributes.
+	The attributes will have the type that the main.Configurations<index> has, otherwise it defaults to the default.
+	The code will listen in a coroutine if the attribute gets changed and will update the main.Configurations<index><value> accordingly.
+	]]
+	function main:SetUpAttributeConfigurations() : nil
+		for _, attribute : string in pairs(main.Configurations.Attributes) do
+			local parsedValue : tabe = string.split(attribute, "/")
+			local storedValue : any = nil
+			local attributeType : string = nil
+			local maxIterations : number = table.maxn(parsedValue)
+			local fixedNamingScheme : string = ""
+			
+			-- Gets the main.Configurations config from the parsed value
+			for i = 1, maxIterations do
+				if i == 1 then
+					storedValue = main.Configurations[parsedValue[i]]
+					fixedNamingScheme = parsedValue[i]
+				else
+					storedValue = storedValue[parsedValue[i]]
+					fixedNamingScheme = fixedNamingScheme .. "_" .. parsedValue[i]
+				end
+			end
+			
+			attributeType = type(storedValue) -- Gets the lua type of the value
+			script:SetAttribute(fixedNamingScheme, storedValue) -- Creates the attribute
+			
+			-- Creates a coroutine to listen for if the attribute changes, and then sets the main.Configuration<index> to the value
+			local function listenToAttribute() : nil
+				storedValue = script:GetAttribute(fixedNamingScheme)
+				
+				-- Adds type checking to make sure that the new value will be the same type as the old value
+				if attributeType ~= nil then
+					if type(storedValue) ~= attributeType then
+						return
+					end
+				end
+				
+				-- Changes the value in main.Configurations<index(es)>. This is a hard-coded code block.
+				if maxIterations == 1 then
+					main.Configurations[parsedValue[1]] = storedValue
+				elseif maxIterations == 2 then
+					main.Configurations[parsedValue[1]][parsedValue[2]] = storedValue
+				elseif maxIterations == 3 then
+					main.Configurations[parsedValue[1]][parsedValue[2]][parsedValue[3]] = storedValue
+				elseif maxIterations == 4 then
+					main.Configurations[parsedValue[1]][parsedValue[2]][parsedValue[3]][parsedValue[4]] = storedValue
+				else
+					warn(script:GetFullName() .. ".main:SetUpAttributeConfigurations():listenToAttribute() could not change main.Configuration<indexes> because the nested configuration is too deep. Please add availability to nest: " .. maxIterations .. " deep.")
+				end
+			end
+			script:GetAttributeChangedSignal(fixedNamingScheme):Connect(listenToAttribute)
+		end
+	end
+	
+	--[[
+	Adds functionality to change main.Configurations.EnemyFolders
+	Accepts overloads:
+	option : boolean → true: add a value to main.Configurations.EnemyFolders, false: remove a value from main.Configurations.EnemyFolders
+	value : Instance | table → Instance: Adds the instance directly to the folder, table will attempt to find the instance through the workspace hierarchy
+	using strings for names. If not every value is a string, the entire search will be thrown out. Any issues with this proccess will throw a warning.
+	]]
+	script.ChangeEnemyTable.Event:Connect(function(option : boolean, value : Folder | table)
+		local toBeChangedFolder : Folder
+		if typeof(value) == "Instance" then
+			toBeChangedFolder = value
+		elseif typeof(value) == "table" then
+			local tempFolder : Instance = nil
+			for i, v in pairs(value) do
+				if type(v) ~= "string" then
+					warn(script:GetFullName() .. ".script.ChangeEnemyTable.Event recieved overload 'value' with a non-string value inside the table.")
+					tempFolder = nil
+					break
+				end
+				
+				if i == 1 then
+					tempFolder = workspace:FindFirstChild(v)
+				else
+					tempFolder = tempFolder:FindFirstChild(v)
+				end
+				if tempFolder == nil then
+					warn(script:GetFullName() .. ".script.ChangeEnemyTable.Event tried to find a Folder with name: " .. v .. ". However, it could not be found.")
+					break
+				end
+			end
+			
+			toBeChangedFolder = tempFolder
+		else
+			warn(script:GetFullName() .. ".script.ChangeEnemyTable.Event recieved an unusable 'value' overload. Recieved value: ", value, " (with type: " .. typeof(value) .. ").")
+		end
+		
+		-- Do nothing if toBeAddedFolder is nil
+		if toBeChangedFolder == nil then
+			return
+		end
+		
+		-- If 'option' is true, add the folder to main.Configurations.EnemyFolders, otherwise try to remove the value. Will throw a warning if the value cannot be found inside the table.
+		if option == true then
+			table.insert(main.Configurations.EnemyFolders, toBeChangedFolder)
+		elseif option == false then
+			local index = table.find(main.Configurations.EnemyFolders, toBeChangedFolder) -- Tries to find the index the folder is at
+			
+			if index then -- Remove the folder at the index
+				table.remove(main.Configurations.EnemyFolders, index)
+			else -- If the flolder was not found, throw an warning
+				warn(script:GetFullName() .. ".script.ChangeEnemyTable.Event could not find folder '" .. toBeChangedFolder:GetFullName() .. "' inside main.Configurations.EnemyFolders.")
+			end
+		end
+	end)
+end
 
 --[[
 Function used to update main.EnemyTable via main.Configurations.EnemyFolders if main.Configurations.EnemyTableUpdateDelay seconds has passed since it was lasted updated.
@@ -189,6 +313,11 @@ function main:UpdateEnemyTable() : boolean
 	
 	return false
 end
+
+-- Tries to run the code for main.Configuratons.AllowAdjustableSettings. We don't care if it fails.
+local ignore, ignored = pcall(function()
+	main:SetUpAttributeConfigurations()
+end)
 
 while task.wait() do
 	main:UpdateEnemyTable()
