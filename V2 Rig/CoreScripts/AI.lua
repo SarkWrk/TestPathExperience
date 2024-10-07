697db8851b934130672ab437a569b523ad191478
Zèe <170918125+SarkWrk@users.noreply.github.com>
2024-10-06 18:02:53 -0700

    Update AI.lua

--- V2 Rig/CoreScripts/AI.lua
+++ V2 Rig/CoreScripts/AI.lua
@@ -1,25 +1,40 @@
-local moduleScript = require(game:GetService("ServerStorage"):WaitForChild("AIComponents"):WaitForChild("AI"))
+local moduleScript = require(game:GetService("ServerStorage"):WaitForChild("Components"):WaitForChild("AIComponents"):WaitForChild("AI"))
 
 local weaponInformation = {}
 
 weaponInformation.Gun = {
 	TypeOfBullet = 2, -- 1 : Raycast, 2 : Part
 	Damage = 10, -- In HP
-	ShotDelay = 0.06, -- In seconds
+	ShotDelay = 10000000, -- In bullets per minute
 	AmountOfShots = 1, -- In bullets
 	ShotsPerBurst = 3, -- In amount of bullets per shot
 	DelayBetweenBurst = nil, -- Leave nil if not in burst
 	Range = 25000, -- In studs
-	BulletDrop = 0.5, -- In studs per second, only used when TypeOfBullet type is NOT 1
+	BulletDrop = 0, -- In studs per second, only used when TypeOfBullet type is NOT 1
 	XSpread = 3, -- In degrees, +/-x
 	YSpread = 3, -- In degrees, +/-y
-	BulletSpeed = 1000, -- In studs per second, only used if TypeOfBullet is NOT 1
+	BulletSpeed = 10000, -- In studs per second, only used if TypeOfBullet is NOT 1
 	ReloadSpeed = 1, -- In seconds
-	MagazineSize = 150, -- Bullets per magazine
-	ReserveSize = 10000, -- Total bullets that can be shot
+	MagazineSize = 1500, -- Bullets per magazine
+	ReserveSize = math.huge, -- Total bullets that can be shot
 	PierceAmount = 0, -- In amount of parts to pierce, how many parts can be pierced by the bullet, only used if TypeOfBullet is NOT 1
 	PierceFallOffDamage = 1, -- In %, how much damage to take off when piercing a part
 }
+
+weaponInformation.Utility = {
+	CanUseGrenade = true, -- bool
+	GrenadeUseDelay = 2, -- In seconds
+}
+
+weaponInformation.Utility.GrenadeStatistics = {
+	Damage = 50, -- In HP
+	Range = 15, -- In studs
+	FallOffDueToDistance = 10, -- %damage to take off per 10 studs
+	FallOffDueToObjects = 10, -- %damage to take off
+	Timer = 5, -- In seconds
+	Instances = {"Goal", "AI"}, -- A table
+}
+
 
 local rig = script.Parent.Parent
 local diedEvent = script.Parent.RigDied
@@ -27,7 +42,20 @@ local stateManager = script.Parent.StateManager
 local firedEvent = script.Shot
 local shootFromLocation = script.Parent.Parent.Head
 
-local AI = moduleScript.interface.New(script, rig, diedEvent, script, script, stateManager, firedEvent, shootFromLocation, weaponInformation)
+local setUpInformation = {
+	["owner"] = script,
+	["rig"] = script.Parent.Parent,
+	["diedEvent"] = script.Parent.RigDied,
+	["stateManagerScript"] = script.Parent.StateManager,
+	["firedEvent"] = script.Shot,
+	["locationToShootFrom"] = script.Parent.Parent.Head,
+	["difficulty"] = 500, -- 500 = normal
+	["shootingScript"] = script,
+	["pathfindingScript"] = script,
+	["weaponInformation"] = weaponInformation
+}
+
+local AI = moduleScript.interface.New(setUpInformation)
 
 local combatAI = AI.Combat
 local pathfindingAI = AI.Pathfinding
@@ -73,6 +101,8 @@ local shootingCoroutine = coroutine.create(function()
 	end
 
 	script:SetAttribute("CanSeeEnemy", false) -- Sets the script's CanSeeEnemy attribute to false so that the pathfinding script won't be effected
+	
+	combatAI.CleanUp(combatAI)
 end)
 
 coroutine.resume(pathfindingCoroutine)
@@ -85,3 +115,11 @@ while task.wait() do
 end
 
 AI.CleanUp()
+
+while task.wait() do
+	if combatAI.Shutoff == true and pathfindingAI.Shutoff == true then
+		break
+	end
+end
+
+script:SetAttribute("Shutoff", true)
