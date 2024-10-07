048c5c5f5117c785c3cb38d6593d504f3696b6eb
Zèe <170918125+SarkWrk@users.noreply.github.com>
2024-10-06 19:21:18 -0700

    Update AI.lua

--- Components/AIComponents/AI.lua
+++ Components/AIComponents/AI.lua
@@ -3,22 +3,53 @@ class.interface = {}
 class.schema = {}
 class.metatable = {__index = class.schema}
 
-function class.interface.New(owner : Script, rig : Model, diedEvent : BindableEvent, pathfindingScript : Script, shootingScript : Script, stateManagerScript : Script, firedEvent : BindableEvent,
-	locationToShootFrom : Part, weaponInformation : table
-)
+function class.interface.New(info : {
+	owner : Script, rig : Model, diedEvent : BindableEvent, locationToShootFrom : string, firedEvent : BindableEvent, difficulty : number, shootingScript : Script, stateManagerScript : Script,
+	weaponInformation : {
+		Gun : {
+			TypeOfBullet : number, Damage : number, ShotDelay : number, AmountOfShots : number, ShotsPerBurst : number?, DelayBetweenBurst : number?, Range : number, BulletDrop : number, XSpread : number, YSpread : number, BulletSpeed : number, ReloadSpeed : number, MagazineSize : number, ReserveSize : number, PierceAmount : number, PierceFallOffDamage : number},
+		Utility : {
+			CanUseGrenade : boolean, GrenadeUseDelay : number,
+			GrenadeStatistics : {
+				Damage : number, Range : number, FallOffDueToDistance : number, FallOffDueToObjects : number, Timer : number, Instances : {}}?},},
+	combatInformation : {
+		Configurations : {
+			AllowAdjustableSettings : boolean, VisualCheckDelay : number, RaycastStart : string, EnemyTableUpdateDelay : number, ViewDistance : number, ViewRadius : number, EnemyTags : {},
+			WeaponConfigurations : {
+				MeleeAvailable : boolean, GunAvailable : boolean, NewTargetChance : number,
+				GunScoreMultipliers : {
+					DistanceScoreMultiplier : number, HealthScoreMultiplier : number, ThreatLevelScoreMultiplier : number, DefenseScoreMultiplier : number},
+				ShootingRaycastParams : {
+					FilterType : string, FilterDecendents : {}},},
+			["RaycastParams"] : {
+				FilterType : string, RespectCanCollide : boolean, IgnoreInViewChecking : {}},
+			Attributes : {},},},
+	pathfindingInformation : {
+		PathfindingInformation : {
+			Goals : {}, AgentRadius : number, AgentHeight : number, WaypointSpacing : number, LabelCosts : {}, JumpHeight : number, MoveSpeed : number, BannedFolders : {Instances}, SkipClosestChance : number, RecheckPossibleTargets : number, Failureinformation : {
+				ExhaustTime : number, RecalculatePath : boolean, ForcePathfinding : boolean}},
+		VisualisationInformation : {
+			VisualisePath : boolean, VisualisationSpacing : number, NormalNodeSize : number, JumpNodeSizeMultiplier : number, CustomNodeSizeMultiplier : number, VisualiseChoosing : boolean, ShowChoosingCircle : boolean, ChoosingCircleExpansionDelay : number, HeightAppearenceWaitTime : number}?,
+		ShootingFunctions : {
+			ShouldHaltOnSeenenemy : boolean, WalkspeedReduction : number, GrenadeAvoidanceRange : number},
+	},})
 	local self = setmetatable({}, class.metatable)
-	self.Combat = require(script.Parent:WaitForChild("CombatAI")).interface.New(owner, rig, diedEvent, locationToShootFrom, weaponInformation, firedEvent)
-	self.Pathfinding = require(script.Parent:WaitForChild("PathfindingAI")).interface.New(owner, rig, shootingScript, diedEvent, stateManagerScript)
+	self.Combat = require(script.Parent:WaitForChild("CombatAI")).interface.New(info.owner, info.rig, info.combatInformation, info.diedEvent, info.locationToShootFrom, info.weaponInformation, info.firedEvent, info.difficulty)
+	self.Pathfinding = require(script.Parent:WaitForChild("PathfindingAI")).interface.New(info.owner, info.rig, info.pathfindingInformation, info.shootingScript, info.diedEvent, info.stateManagerScript)
 	
 	return self
 end
 
 function class.schema.CleanUp(self : BaseAI)
-	self.Combat:CleanUp()
-	self.Pathfinding:CleanUp()
+	local _, _ = pcall(function()
+		self.Combat:CleanUp()
+	end)
+	local _, _ = pcall(function()
+		self.Pathfinding:CleanUp()
+	end)
 end
 
-type BaseAI = typeof(class.interface.New(table.unpack(...)))
+export type BaseAI = typeof(class.interface.New(table.unpack(...)))
 
 
 return class
