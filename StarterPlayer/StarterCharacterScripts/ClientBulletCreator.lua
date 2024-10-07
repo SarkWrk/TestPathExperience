12f8d061926717a2cd9fa0f1638e38b31e4a1497
Zèe <170918125+SarkWrk@users.noreply.github.com>
2024-10-07 16:18:24 -0700

    Update ClientBulletCreator.lua

--- StarterPlayer/StarterCharacterScripts/ClientBulletCreator.lua
+++ StarterPlayer/StarterCharacterScripts/ClientBulletCreator.lua
@@ -1,18 +1,7 @@
 local tracker : {[number] : {Part : Part, Tween : Tween}} = {}
 local helper = require(game:GetService("ReplicatedStorage"):WaitForChild("Helpers"):WaitForChild("GeneralLibrary"))
-
-function StringSplitter(toBeSplit : string, seperator : string?) : {}
-	if seperator == nil then
-		seperator = ","
-	end
-	
-	return string.split(toBeSplit, seperator)
-end
-
-function FixVector3(toBeFixed : string) : Vector3
-	local splitInformation = StringSplitter(toBeFixed)
-	return Vector3.new(splitInformation[1], splitInformation[2], splitInformation[3])
-end
+local byteNet = require(game:GetService("ReplicatedStorage"):WaitForChild("ByteNet"):WaitForChild("ByteNet"))
+local packets = require(game:GetService("ReplicatedStorage"):WaitForChild("ByteNet"):WaitForChild("Packets"))
 
 function RemoveBullet(index : number) : nil
 	if tracker[index] ~= nil then
@@ -24,29 +13,25 @@ function RemoveBullet(index : number) : nil
 	end
 end
 
-game:GetService("ReplicatedStorage").MoveBullets.OnClientEvent:Connect(function(eventType : string, information : {Index : number, Position : string, EndPosition : string, Speed : number, Rotation : string} | number)
-	if eventType == "D" then
-		RemoveBullet(information)
-	elseif eventType == "C" then
-		local rotation = StringSplitter(information.Rotation)
-		
+packets.BulletCreation.BulletCreation.listen(function(data)
+	if data.Setting == "D" then
+		RemoveBullet(data.Index)
+	elseif data.Setting == "C" then
 		local bullet = game:GetService("ReplicatedStorage").Bullet:Clone()
 		bullet.Parent = workspace.Bullets
-		bullet.CFrame = CFrame.new(FixVector3(information.Position)) * CFrame.Angles(rotation[1], rotation[2], rotation[3])
+		bullet.CFrame = helper.Conversions.Vector3ToCFrame(helper.Conversions.StringToVector3(data.Position, ","), helper.Conversions.StringToVector3(data.Rotation, ","))
 
-		tracker[information.Index] = {}
+		tracker[data.Index] = {}
 
-		local tween = game:GetService("TweenService"):Create(bullet, TweenInfo.new(helper.Delta(FixVector3(information.Position), FixVector3(information.EndPosition))/information.Speed, Enum.EasingStyle.Linear), {Position = FixVector3(information.EndPosition)})
-		tween:Play()
+		tracker[data.Index].Tween = game:GetService("TweenService"):Create(bullet, TweenInfo.new(helper.Delta(helper.Conversions.StringToVector3(data.Position, ","), helper.Conversions.StringToVector3(data.EndPosition, ","))/data.Speed, Enum.EasingStyle.Linear), {Position = helper.Conversions.StringToVector3(data.EndPosition, ",")})
+
+		tracker[data.Index].Part = bullet
 		
-		tween.Completed:Connect(function()
-			RemoveBullet(information.Index)
+		tracker[data.Index].Tween:Play()
+		tracker[data.Index].Tween.Completed:Connect(function()
+			RemoveBullet(data.Index)
 		end)
-
-		tracker[information.Index].Part = bullet
-		tracker[information.Index].Tween = tween
-		
 	else
-		warn("EventType '" .. eventType .. "' not registered.")
+		warn("Setting '" .. data.Setting .. "' not registered.")
 	end
 end)
