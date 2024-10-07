126e381ec337727caee7f195305e11dde1738e3c
Zèe <170918125+SarkWrk@users.noreply.github.com>
2024-10-07 16:18:02 -0700

    Update ManagerScript.lua

--- ServerScriptService/BulletManager/BulletManager/ManagerScript.lua
+++ ServerScriptService/BulletManager/BulletManager/ManagerScript.lua
@@ -1,66 +1,57 @@
+--!strict
+
 local bulletComponent = require(game:GetService("ServerStorage"):WaitForChild("Components"):WaitForChild("BulletComponent"):WaitForChild("BulletComponent"))
+local byteNet = require(game:GetService("ReplicatedStorage"):WaitForChild("ByteNet"):WaitForChild("ByteNet"))
+local packets = require(game:GetService("ReplicatedStorage"):WaitForChild("ByteNet"):WaitForChild("Packets"))
 
 local bulletIndex = 0
-local activeBullets : {[number] : {["lookdirection"] : string}} = {}
+local activeBullets : {[number] : {["lookdirection"] : Vector3}} = {}
 local event = game:GetService("ReplicatedStorage").MoveBullets
 local totalActiveBullets = 0
 local helper = require(game:GetService("ReplicatedStorage"):WaitForChild("Helpers"):WaitForChild("GeneralLibrary"))
 
+
+local function SendToClients(setting : string, sendData : {Index : number, Position : Vector3?, EndPosition : Vector3?, Speed : number?, Rotation : Vector3?})
+	packets.BulletCreation.BulletCreation.sendToAll({
+		Setting = setting,
+		Index = sendData.Index,
+		Position = helper.Conversions.Vector3ToString(sendData.Position, ",", true, 3),
+		EndPosition = helper.Conversions.Vector3ToString(sendData.EndPosition, ",", true, 3),
+		Speed = sendData.Speed,
+		Rotation = helper.Conversions.Vector3ToString(sendData.Rotation, ","),
+	})
+end
+
 local function BulletCreation(info : {})
 	coroutine.resume(coroutine.create(function()
-		--[[Converts a CFrame to a string equivilant of its rotation
-		Accepts overloads of:
-		converted <CFrame> → The CFrame that should be converted to a string of its rotation
-		seperator <string> → The seperator between the X, Y, and Z rotation
-		]]
-		local function ConvertCFrameToString(converted : CFrame, seperator : string?) : string
-			if seperator == nil then
-				seperator = ","
-			end
-
-			local angleX, angleY, angleZ = converted:ToEulerAnglesXYZ()
-			
-			return tostring(helper.Rounder(angleX, 2)) .. seperator .. tostring(helper.Rounder(angleY, 2)) .. seperator .. tostring(helper.Rounder(angleZ, 2))
-		end
-		
-		--[[Converts a Vector3 to a string representation
-		Accepts overloads of:
-		vector <Vector3> → The Vector3 that should be converted to a string
-		seperator <string> → The seperator between each positional axis
-		]]
-		local function ShortenVector3(vector : Vector3, seperator : string?) : string
-			if seperator == nil then
-				seperator = ","
-			end
-			return helper.Rounder(vector.X, 3) .. seperator .. helper.Rounder(vector.Y, 3) .. seperator .. helper.Rounder(vector.Z, 3)
-		end
-
 		-- Create an index in activeBullets to track the bullet that has been fired
 		local index = bulletIndex
 		bulletIndex += 1
 		local bullet = bulletComponent.interface.New(info)
 		local lookAt = bullet.Variable.LookDirection
 
-		activeBullets[index] = {lookdirection = ConvertCFrameToString(bullet.Variable.LookDirection)}
+		activeBullets[index] = {lookdirection = helper.Conversions.CFrameToVector3(bullet.Variable.LookDirection, true, 2)}
 
 		local heartbeat
 		
 		totalActiveBullets += 1
 		
+		local endPosition = (bullet.Variable.LookDirection * CFrame.Angles(0, math.rad(-90), 0)).LookVector * bullet.Information.DistanceTimeOut
+		
 		-- Prepares some data to be sent to the client for bullet creation
 		local sendInformation = {}
 		sendInformation.Index = index
-		sendInformation.Position = ShortenVector3(bullet.Information.StartPosition)
-		sendInformation.EndPosition = ShortenVector3((bullet.Variable.LookDirection * CFrame.Angles(0, math.rad(-90), 0)).LookVector * bullet.Information.DistanceTimeOut)
+		sendInformation.Position = bullet.Information.StartPosition
+		sendInformation.EndPosition = endPosition - Vector3.new(0, helper.Delta(bullet.Information.StartPosition, endPosition)/bullet.Information.Speed * bullet.Information.BulletDrop, 0)
 		sendInformation.Speed = bullet.Information.Speed
-		sendInformation.Rotation = ConvertCFrameToString(bullet.Variable.LookDirection)
+		sendInformation.Rotation = helper.Conversions.CFrameToVector3(bullet.Variable.LookDirection, true, 2)
 
 		task.synchronize()
-		event:FireAllClients("C", sendInformation)
+		SendToClients("C", sendInformation)
 
 		-- Monitor the bullet for specific information
 		heartbeat = game:GetService("RunService").Heartbeat:ConnectParallel(function()
-			activeBullets[index].lookdirection = ConvertCFrameToString(bullet.Variable.LookDirection)
+			activeBullets[index].lookdirection = helper.Conversions.CFrameToVector3(bullet.Variable.LookDirection, true, 2)
 
 			-- When the bullet is destroyed, tell the client to also destroy the bullet
 			if bullet.Variable.ToDestroy == true then
@@ -68,7 +59,7 @@ local function BulletCreation(info : {})
 
 				task.synchronize()
 				
-				event:FireAllClients("D", index)
+				SendToClients("D", {Index = index})
 
 				heartbeat:Disconnect()
 			end
